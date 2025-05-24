f6e28b44680095df439ec8e6a7b17fe5047d8447
Tim <obotetimothy005@gmail.com>
2025-05-24 18:14:07 +0300

    updated files

---
+++ button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Button({ children, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+    >
+      {children}
+    </button>
+  );
+}
---
+++ input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Input({ type = "text", placeholder, value, onChange }) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="border border-gray-300 rounded px-3 py-2 w-full"
+    />
+  );
+}
